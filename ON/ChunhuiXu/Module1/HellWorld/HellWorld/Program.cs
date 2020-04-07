427e8d51532baeb894cfdededa2a52ef2d863c27
pulafei <1847813772@qq.com>
2020-04-06 17:55:04 -0700

    My work of DataTypes&Variables

---
+++ ON/ChunhuiXu/Module1/HellWorld/HellWorld/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace HellWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("");
+
+            Console.WriteLine("    /|");
+            Console.WriteLine("   / |");
+            Console.WriteLine("  /  |");
+            Console.WriteLine(" /   |");
+            Console.WriteLine("/____|");
+            Console.WriteLine("");
+
+            String Name = "Chunhui Xu";
+            int Age = 23;
+            bool isFemale = false;
+
+            Console.WriteLine(Name);
+            Console.WriteLine(Age);
+            Console.WriteLine(isFemale);
+            Console.WriteLine("");
+
+            double MyFloat = 10.1;
+            Console.WriteLine(MyFloat);
+
+        }
+    }
+}
