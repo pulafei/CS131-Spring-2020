f707fce3bf7157a1100f9819b62e59911f993f5d
Mary Oh <mary.christine.oh@gmail.com>
2020-04-13 15:08:57 -0700

    part 2 2d array initialization

---
+++ ON/MaryOh/Module2/Arrays-Enums/2DArrays/2DArrays/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _2DArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int rows = 3;
+            int columns = 3;
+
+            int[,] table = new int[rows, columns]; // creates a 2D array that is 3x3
+            table[0, 0] = 1;
+            table[0, 1] = 2;
+            table[0, 2] = 3;
+
+            // loop through and print the whole 2D array
+            for (int x = 0; x < rows; x++) {
+                for (int y = 0; y < columns; y++) {
+                    Console.Write(table[x, y]);
+                }
+                Console.WriteLine(); // a new line is created after each row is iterated
+            }
+        }
+    }
+}
