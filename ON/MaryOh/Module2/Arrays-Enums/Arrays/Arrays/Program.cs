ae5d26c50b6dd4683563bb68bda2dac42f1d23d5
Mary Oh <mary.christine.oh@gmail.com>
2020-04-13 15:01:53 -0700

    challenge 1

--- ON/MaryOh/Module2/Arrays-Enums/Arrays/Arrays/Program.cs
+++ ON/MaryOh/Module2/Arrays-Enums/Arrays/Arrays/Program.cs
@@ -6,21 +6,33 @@ namespace Arrays
     {
         static void Main(string[] args)
         {
-            int[] numbers = new int[5]; // creating a new array of type integer named 'numbers'
-            numbers[0] = 1;
-            numbers[1] = 2;
-            numbers[2] = 3;
-            numbers[3] = 4;
-            numbers[4] = 5;
+            //int[] numbers = new int[5]; // creating a new array of type integer named 'numbers'
+            //numbers[0] = 1;
+            //numbers[1] = 2;
+            //numbers[2] = 3;
+            //numbers[3] = 4;
+            //numbers[4] = 5;
 
-            // print out the whole array
-            //for (int i = 0; i < numbers.Length; i++) {
-            //    Console.Write(numbers[i] + " ");
+            //// print out the whole array
+            ////for (int i = 0; i < numbers.Length; i++) {
+            ////    Console.Write(numbers[i] + " ");
+            ////}
+
+            //foreach (int number in numbers) {
+            //    Console.Write(number + " ");
             //}
 
-            foreach (int number in numbers) {
-                Console.Write(number + " ");
+            string[] Sentence = new string[] {
+                "This",
+                "is",
+                "a",
+                "string"
+            };
+
+            foreach (string word in Sentence) {
+                Console.Write(word + " ");
             }
         }
     }
 }
+
