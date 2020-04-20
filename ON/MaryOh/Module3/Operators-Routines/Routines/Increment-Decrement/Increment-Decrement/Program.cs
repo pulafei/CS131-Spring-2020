5e01c896b73c1ccb91e2690c982de7d217c57760
Mary Oh <mary.christine.oh@gmail.com>
2020-04-20 13:53:51 -0700

    initialized part 2

---
+++ ON/MaryOh/Module3/Operators-Routines/Routines/Increment-Decrement/Increment-Decrement/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Increment_Decrement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int a = 0; a <= 10; a++) {
+                Console.WriteLine(a);
+            }
+        }
+    }
+}
