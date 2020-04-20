b59bedcd1e588a7427c4c0e80b817f6e54f1a414
Priyanka Kamdar <pkamdar27@gmail.com>
2020-04-19 18:14:04 -0700

    HOP02 Arrays and Enumeration

---
+++ ON/PriyankaKamdar/Module2/Arrays/Arrays/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] sentence = new string[4]; //creating a new array of type integer named 'numbers'
+            sentence [0] = "This";
+            sentence [1] = "is";
+            sentence [2] = "a";
+            sentence [3] = "string";
+            
+            foreach (string word in sentence)
+            {
+                Console.Write(word + " ");            
+            }
+            Console.WriteLine();
+        }
+    }
+}
