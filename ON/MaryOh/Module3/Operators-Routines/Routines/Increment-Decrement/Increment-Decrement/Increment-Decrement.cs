f22e99464d6c82c50a178143ce6304e2efa0786a
Mary Oh <mary.christine.oh@gmail.com>
2020-04-20 14:03:53 -0700

    Includes challenge 3

--- ON/MaryOh/Module3/Operators-Routines/Routines/Increment-Decrement/Increment-Decrement/Increment-Decrement.cs
+++ ON/MaryOh/Module3/Operators-Routines/Routines/Increment-Decrement/Increment-Decrement/Increment-Decrement.cs
@@ -9,6 +9,10 @@ namespace Increment_Decrement
             for (int a = 0; a <= 10; a++) { // iterating from 0 to 10
                 Console.WriteLine(a); // printing each iteration on a new line
             }
+
+            for (int i = 15; i >= 1; i--) { // iterating from 15 to 1
+                Console.WriteLine(i); // printing each iteration on a new line
+            }
         }
     }
 }
