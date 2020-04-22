36829c7ac86339feb59ae8b843796f5a1571a39b
hollyminch <63515777+hollyminch@users.noreply.github.com>
2020-04-21 17:45:03 -0700

    Submit Module 1 and 2

---
+++ ON/Holly Minch/Module 1/PE01 Coding and Debugging/hello.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace PE01_Coding_and_Debugging
+{
+    class Hello
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.Read();
+
+        }
+    }
+}
---
+++ ON/Holly Minch/Module 2/Enums/Enums/Enums/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Enums
+{
+    enum Gender {Male, Female, Other};
+    enum Age {Young, Middle, Old};
+    enum Hair {Long, Short, Bald };
+    enum Height {Tall, Short};
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" I am A"  + Gender.Female);
+            Console.WriteLine("My Age is"  + Age.Young);
+            Console.WriteLine("My Hair is" + Hair.Long);
+            Console.WriteLine("My Height is"  + Height.Short);
+        }
+    }
+}
