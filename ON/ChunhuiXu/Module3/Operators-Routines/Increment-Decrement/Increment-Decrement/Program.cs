6ef385c3051e6c8cbfce1cf75b86441a89a68412
pulafei <1847813772@qq.com>
2020-04-25 13:40:50 -0700

    HOP03

---
+++ ON/ChunhuiXu/Module3/Operators-Routines/Increment-Decrement/Increment-Decrement/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Increment_Decrement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Output 0 to 10");
+            for (int a = 0; a <= 10; a++)          // if a <= 10 , a++.
+            {
+                Console.WriteLine(a);              // output a and make a new line
+            }
+            Console.WriteLine();                   // make a new line
+            Console.WriteLine("Output 15 to 1");  
+            for(int b = 15; b > 0; --b)            // if b > 0 , --b
+            {
+                Console.WriteLine(b);              // output b and make a new line
+            }
+            Console.ReadKey();                     // finish
+        }
+    }
+}
---
+++ ON/ChunhuiXu/Module3/Operators-Routines/NumericOperators/NumericOperators/NumericOperators/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+
+namespace NumericOperators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 20;                                   // input data of num1,2,3
+            int num2 = 77;
+            float num3 = 5.2f;
+
+            float sum = num1 + num2 + num3;                  // Addition
+            int subtract = num2 - num1;                      // Subtraction
+            int multiply = num1 * num2;                      // Multiplication
+            float divide = num1 / num3;                      // Division
+            int modulus = num2 % num1;                       // Remainder
+
+            Console.WriteLine("Sum if 3 numbers is : " + sum);    // output the results
+            Console.WriteLine("{0} - {1} is : {2}", num2, num1, subtract);
+            Console.WriteLine("{0} * {1} is : {2}", num1, num2, multiply);
+            Console.WriteLine("{0} / {1} is : {2}", num1, num3, divide);
+            Console.WriteLine("{0} % {1} is : {2}", num2, num1, modulus);
+            Console.ReadKey();                                    // Finish
+        }
+    }
+}
