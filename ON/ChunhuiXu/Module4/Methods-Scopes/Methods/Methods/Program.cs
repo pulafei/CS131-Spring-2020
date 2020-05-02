ebaa8077eaff7bbfed724ea91f1ef9851e4a16f4
pulafei <1847813772@qq.com>
2020-05-02 15:50:00 -0700

    HOP04 Chunhui Xu

---
+++ ON/ChunhuiXu/Module4/Methods-Scopes/Methods/Methods/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Reflection.Metadata.Ecma335;
+
+namespace Methods
+{
+    class Program
+    {
+        // this is a method called "PrintSorthing"
+        public static void PrintSomething()
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        // a method called "Adding"
+        public static int Adding(int num1, int num2)
+        {
+            int sum = num1 + num2;
+            return sum;   // make a sum and return
+        }
+        // Challenge
+        // a method called "Mod"
+        public static double Mod(double a, double b = 1)
+        {
+            double div = a / b;
+            return div;        // after a/b, return div
+        }
+        // this is a method called "Main"
+        static void Main(string[] args)
+        {
+            // PrintSomething();  // method "PrintSomething" 
+            Console.WriteLine(Adding(10, 20));  // output the result of "Adding"
+
+
+            Console.WriteLine(Mod(20, 13));  // output the result of "Mod"
+            Console.ReadKey();      // finish
+           
+
+        }
+    }
+}
---
+++ ON/ChunhuiXu/Module4/Methods-Scopes/Scopes/Scopes/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Scopes
+{
+    class Program
+    {
+        public static string Str2; // "Str2" is null
+        static void ReturnString(string Str1)
+        {
+        
+            string Str2 = "Programming in C#";
+            Console.Write(Str1);
+            Console.WriteLine(Str2);
+        }
+        static void Main(string[] args)
+        {
+            ReturnString("CS131 ");
+            Console.WriteLine(Str2);
+        }
+    }
+}
