dfa6f709dbb7b8eaef6226ef082e9d80e576506f
pulafei <1847813772@qq.com>
2020-05-07 14:52:10 -0700

    HOP05 Chunhui Xu

---
+++ ON/ChunhuiXu/Module5/Conditions-Loops/Conditional-Statements/Conditional-Statements/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Conditional_Statements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 1;
+            Console.WriteLine("Guess a number (1 - 10)");
+            int userInput = Convert.ToInt32(Console.ReadLine()); //input the value
+            
+            if (userInput > 10 || userInput < 1)
+            {
+                Console.WriteLine("Number must be between 1 - 10!!!");
+            }
+            else if (userInput == num)
+            {
+                Console.WriteLine("your guess was correct!");
+            }
+            else if (userInput != num)
+            {
+                Console.WriteLine("You guessed wrong. Goodluck next time!");
+            }
+        }
+    }
+}
---
+++ ON/ChunhuiXu/Module5/Conditions-Loops/While-Loop/While-Loop/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace While_Loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 1;
+            int userInput = Convert.ToInt32(Console.ReadLine()); // input value
+            while (true)
+            {
+                if (userInput > 10 || userInput < 1)
+                {
+                    Console.Write("Number must be between 1 - 10. Guess again: ");
+                    userInput = Convert.ToInt32(Console.ReadLine()); // input new value. I prefer they are in same line.
+                }
+                else if (userInput != num)
+                {
+                    Console.Write("You guessed wrong. Guess again: ");
+                    userInput = Convert.ToInt32(Console.ReadLine());  // input new value. I prefer they are in same line.
+                }
+                else if (userInput == num)
+                {
+                    Console.WriteLine("Your guess was correct!");
+                    break;
+                }
+            }
+        }
+    }
+}
